6ed228568afb06d37d5b1f01a7c7c4996a75ac66
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-04-21 15:56:42 -0300

    FrameRequestHelper can now pass data to callback

--- src/frame-request-helper.ts
+++ src/frame-request-helper.ts
@@ -1,19 +1,30 @@
-/** Parameters for {@link FrameRequestHelperCallback}. */
-export interface FrameRequestHelperCallbackParameters {
+/**
+ * Parameters for {@link FrameRequestHelperCallback}.
+ * @template Data Data type to pass to the callback every frame.
+ */
+export interface FrameRequestHelperCallbackParameters<Data = undefined> {
   /** Timestamp where last frame ended. */
   timestamp: number;
   /** Time elapsed since the callback was last called. */
   deltaTime: number;
+  /** Data passed to the callback every frame. */
+  data: Data;
 }
 
-/** Callback to be used with {@link FrameRequestHelper}. */
-export type FrameRequestHelperCallback = (
-  parameters: FrameRequestHelperCallbackParameters,
+/**
+ * Callback to be used with {@link FrameRequestHelper}.
+ * @template Data Data type to pass to the callback every frame.
+ */
+export type FrameRequestHelperCallback<Data = undefined> = (
+  parameters: FrameRequestHelperCallbackParameters<Data>,
 ) => void;
 
-/** Simplifies the use of {@link requestAnimationFrame}. */
-export class FrameRequestHelper {
-  #callback: FrameRequestHelperCallback;
+/**
+ * Simplifies the use of {@link requestAnimationFrame}.
+ * @template Data Data type to pass to the callback every frame.
+ */
+export class FrameRequestHelper<Data = undefined> {
+  #callback: FrameRequestHelperCallback<Data>;
   #requestID?: number | undefined;
   #lastTimestamp?: number | undefined;
 
@@ -22,12 +33,15 @@ export class FrameRequestHelper {
    * @param callback - Callback to run at every frame once {@link start} is
    * called.
    */
-  constructor(callback: FrameRequestHelperCallback) {
+  constructor(callback: FrameRequestHelperCallback<Data>) {
     this.#callback = callback;
   }
 
-  /** Starts running the provided callback at every frame. */
-  start(): void {
+  /**
+   * Starts running the provided callback at every frame.
+   * @param data - Data to pass to the callback every frame.
+   */
+  start(...data: Data extends undefined ? [] : [Data]): void {
     if (this.#requestID != null) return;
 
     const callback = (timestamp: number): void => {
@@ -38,7 +52,7 @@ export class FrameRequestHelper {
       // The next animation frame is requested before callback runs, so that the
       // callback itself can cancel it by using the instance methods.
       this.#requestID = requestAnimationFrame(callback);
-      this.#callback({ timestamp, deltaTime });
+      this.#callback({ timestamp, deltaTime, data: data[0] as Data });
     };
 
     this.#requestID = requestAnimationFrame(callback);
